c06a3910dbed4106aa79be322131291c4f4e5908
Rinsath07 <rinshad6263@gmail.com>
2024-05-16 22:26:49 +0530

    small

--- src/test/java/HappyPath.java
+++ src/test/java/HappyPath.java
@@ -1,13 +1,26 @@
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 
 public class HappyPath extends TestBase{
     @Test
-    public void login()
-    {
+    public void login() throws AWTException, IOException {
       DriverManager.getInstance().getDriver().get(propertyManager.getInstance().get_Baseurl()+Endpoints.getInstance().get_login_endpoint());
         System.out.println("happy path : "+Thread.currentThread().threadId());
         LoginPage.getInstance().login(propertyManager.getInstance().get_username(), propertyManager.getInstance().get_password());
       HomePage.getInstance().validate_profile();
+        Actions actions=new Actions(null);
+//        Robot robot=new Robot();
+//     Dimension dimension=   Toolkit.getDefaultToolkit().getScreenSize();
+//     Rectangle rectangle=new Rectangle(dimension);
+//   BufferedImage image= robot.createScreenCapture(rectangle);
+//        ImageIO.write(image,"png",new File(""));
     }
 }
--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -1,11 +1,13 @@
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.asserts.SoftAssert;
 
 public class TestBase {
 
     @BeforeTest
     public void testbase()
     {
+
       DriverManager.getInstance().setDriver(BrowserManager.getInstance().createBrowserInstance(propertyManager.getInstance().get_browserName()));
     }
     @AfterTest
